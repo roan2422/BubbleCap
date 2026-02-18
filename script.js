07bf5592e929f086e2bfa8da8306ab85da67c144
roan vanderveen <rvanderveen32@st.noorderpoort.nl>
2026-02-18 11:36:41 +0100

    test

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+/* login */
 
+/* standard prices */
+fetch("stock.json")
+    .then((response) => response.json())
+    .then((data) => {
+        const itemnames = document.getElementsByClassName("item-name");
+        const prices = document.getElementsByClassName("price");
+
+        for (let i = 0; i < itemnames.length; i++) {
+            const name = itemnames[i].textContent;
+
+            const item = data.stock.find((product) => product.name === name);
+
+            if (item) {
+                prices[i].textContent = `$${item.price}`;
+            }
+        }
+    });
